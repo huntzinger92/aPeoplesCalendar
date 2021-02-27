f2e0df4ff7661f3b0c686b96dcaf79e99c1836ed
Trevor Smith <tts2p4@gmail.com>
2021-02-27 17:19:42 -0600

    Finished empty days!

--- App.js
+++ App.js
@@ -28,33 +28,45 @@ import {NotificationTest} from './NotificationTest.js';
 var everyDayString = Object.keys(eventLibrary);
 var emptyCount = 0;
 var totalEventsCount = 0;
+var singleEventCount = 0;
+var dayEventsCount;
 for (var i = 0; i < everyDayString.length; i++) {
   var day = eventLibrary[everyDayString[i]];
+  dayEventsCount = 0;
   //if day has no entries, increment count by one
   if (!day['Revolution'][0].description && !day['Rebellion'][0].description && !day['Labor'][0].description && !day['Birthdays'][0].description && !day['Assassinations'][0].description && !day['Other'][0].description) {
     emptyCount++;
   } else {
     if (day['Revolution'][0].description) {
       totalEventsCount += day['Revolution'].length;
+      dayEventsCount += day['Revolution'].length;
     };
     if (day['Rebellion'][0].description) {
       totalEventsCount += day['Rebellion'].length;
+      dayEventsCount += day['Rebellion'].length;
     };
     if (day['Labor'][0].description) {
       totalEventsCount += day['Labor'].length;
+      dayEventsCount += day['Labor'].length;
     };
     if (day['Birthdays'][0].description) {
       totalEventsCount += day['Birthdays'].length;
+      dayEventsCount += day['Birthdays'].length;
     };
     if (day['Assassinations'][0].description) {
       totalEventsCount += day['Assassinations'].length;
+      dayEventsCount += day['Assassinations'].length;
     };
     if (day['Other'][0].description) {
       totalEventsCount += day['Other'].length;
+      dayEventsCount += day['Other'].length;
     };
+    if (dayEventsCount === 1) {
+      singleEventCount++;
+    }
   };
 };
-console.log('amount of empty days: ' + emptyCount);
+console.log('amount of single event days: ' + singleEventCount);
 console.log('amount of events: ' + totalEventsCount);
 //*/
 
